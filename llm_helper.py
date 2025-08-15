d63ddee004437003ff6e2b86ad84d690f2ba1e1d
Ananyasolanki123 <133773293+Ananyasolanki123@users.noreply.github.com>
2025-08-15 17:14:31 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain_groq import ChatGroq
 
 # Load GROQ API key from Streamlit secrets
-GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
+GROQ_API_KEY = st.secrets["api_keys"]["GROQ_API_KEY"]
 
 llm = ChatGroq(
     groq_api_key=GROQ_API_KEY,
@@ -13,4 +13,3 @@ llm = ChatGroq(
 if __name__ == "__main__":
     response = llm.invoke("What are the main ingredients in a cake?")
     print(response.content)
-
